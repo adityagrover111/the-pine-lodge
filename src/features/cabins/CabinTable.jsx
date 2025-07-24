5449314ea1954d45abf545683241a292750326cc
Aditya Grover <axg1595@mavs.uta.edu>
2025-07-24 22:35:45 +0530

    created reusable components for sorting and filtering and integrated to cabins page

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,13 +5,29 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 
 import Empty from "../../ui/Empty";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("discount") || "all";
 
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
   if (isLoading) return <Spinner />;
   if (!cabins.length) return <Empty resourceName="cabins" />;
+  console.log(filteredCabins);
 
+  const sortBy = searchParams.get("sortBy") || "name-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
   return (
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
@@ -23,7 +39,7 @@ function CabinTable() {
         <div></div>
       </Table.Header>
       <Table.Body
-        data={cabins}
+        data={sortedCabins}
         render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
       />
     </Table>
